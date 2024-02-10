e4876ead108f2b6cf19e17ed3effa125f3d8b903
BowTiedDevOps <157840260+BowTiedDevOps@users.noreply.github.com>
2024-02-10 23:21:11 +0200

    feat: add job to check that tests were successful in more workflows

--- testnet/stacks-node/src/tests/neon_integrations.rs
+++ testnet/stacks-node/src/tests/neon_integrations.rs
@@ -7042,6 +7042,7 @@ fn atlas_integration_test() {
 #[test]
 #[ignore]
 fn antientropy_integration_test() {
+    // commented in order to run tests
     if env::var("BITCOIND_TEST") != Ok("1".into()) {
         return;
     }
